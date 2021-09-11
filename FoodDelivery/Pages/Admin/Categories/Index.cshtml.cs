1deb3f8b8b861a096cb094bcbce2f610c325f6c2
coleminer399 <43429579+coleminer399@users.noreply.github.com>
2021-09-11 16:50:56 -0600

    Week2 Vid15

--- FoodDelivery/Pages/Admin/Categories/Index.cshtml.cs
+++ FoodDelivery/Pages/Admin/Categories/Index.cshtml.cs
@@ -12,18 +12,6 @@ namespace FoodDelivery.Pages.Admin.Categories
 {
     public class IndexModel : PageModel
     {
-        private readonly Infrastructure.Data.ApplicationDbContext _context;
-
-        public IndexModel(Infrastructure.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        public IList<Category> Category { get;set; }
-
-        public async Task OnGetAsync()
-        {
-            Category = await _context.Category.ToListAsync();
-        }
+        
     }
 }
