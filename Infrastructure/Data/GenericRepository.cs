ca8785f2479d72d80bbb886c946c014c0851ce15
coleminer399 <43429579+coleminer399@users.noreply.github.com>
2021-09-11 13:49:42 -0600

    Week1 Vid8

---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,107 @@
+﻿using ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbcontext;
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        //By using ReadOnly ApplicationDbContext, you can have access to only
+        //querying capabilitiies of DbContext. UnitOfWOrk actually writes
+        //(commits) to the PHYSICAL tables (not internal object)
+
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> predicate, bool asNoTracking = false, string includes = null)
+        {
+            if (includes == null)
+            {
+                if (asNoTracking)
+                {
+                    return _dbcontext.Set<T>()
+                        .AsNoTracking()
+                        .Where(predicate)
+                        .FirstOrDefault();
+                }
+                else
+                {
+                    return _dbcontext.Set<T>()
+                        .Where(predicate)
+                        .FirstOrDefault();
+                }
+            }
+            else
+            {
+                IQueryable<T> queryable = _dbcontext.Set<T>();
+                foreach (var includeProperty in includes.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    queryable = queryable.Include(includeProperty);
+                }
+                if (asNoTracking)
+                {
+                    return _dbcontext.Set<T>()
+                        .AsNoTracking()
+                        .Where(predicate)
+                        .FirstOrDefault();
+                }
+                else
+                {
+                    return _dbcontext.Set<T>()
+                        .Where(predicate)
+                        .FirstOrDefault();
+                }
+            }
+        }
+
+        //virtual keyword is used to modify a method, property, indexer, or
+        //and allows for it to be overridden in a derived class.
+        public virtual T GetById(int id)
+        {
+            return _dbcontext.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> List(Expression<Func<T, bool>> predicate, Expression<Func<T, int>> orderBy = null, string includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, int>> orderBy = null, string includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
