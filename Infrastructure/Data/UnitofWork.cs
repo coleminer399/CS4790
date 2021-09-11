7d5b7beea3438090e2cd50f2d2f99956c1551533
coleminer399 <43429579+coleminer399@users.noreply.github.com>
2021-09-11 14:59:58 -0600

    Week1 Vid10

---
+++ Infrastructure/Data/UnitofWork.cs
@@ -0,0 +1,40 @@
+﻿using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class UnitofWork : IUnitofWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public UnitofWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        private IGenericRepository<Category> _Category;
+        public IGenericRepository<Category> Category
+        {
+            get
+            {
+                if (_Category == null) _Category = new GenericRepository<Category>(_dbContext);
+                    return _Category;
+                
+            }
+        }
+
+        public int commit()
+        {
+            return _dbContext.SaveChanges();
+        }
+
+        public async Task<int> commitAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public void Dispose() => _dbContext.Dispose();
+    }
+}
