e003a9cbdeaa390ed153714a65db180437ec3092
coleminer399 <43429579+coleminer399@users.noreply.github.com>
2021-09-11 13:08:44 -0600

    Week1 Vid7

---
+++ ApplicationCore/Interfaces/IUnitofWork.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IUnitofWork
+    {
+        public IGenericRepository<Category> Category { get;}
+        int commit();
+        Task<int> commitAsync();
+    }
+}
