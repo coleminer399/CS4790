da7a0b94b017165dc6e1379d5a2b60055a266470
coleminer399 <43429579+coleminer399@users.noreply.github.com>
2021-09-25 23:00:16 -0600

    Need to move top Net Core 5

---
+++ FoodDelivery/Areas/Identity/Data/FoodDeliveryContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodDelivery.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDelivery.Data
+{
+    public class FoodDeliveryContext : IdentityDbContext<FoodDeliveryUser>
+    {
+        public FoodDeliveryContext(DbContextOptions<FoodDeliveryContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ FoodDelivery/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FoodDelivery.Areas.Identity.Data;
+using FoodDelivery.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FoodDelivery.Areas.Identity.IdentityHostingStartup))]
+namespace FoodDelivery.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FoodDeliveryContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FoodDeliveryContextConnection")));
+
+                services.AddDefaultIdentity<FoodDeliveryUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<FoodDeliveryContext>();
+            });
+        }
+    }
+}
